fe15b324be6fa29d60e9321d3b475a2833b8dfda
Will Stone <654103+will-stone@users.noreply.github.com>
2017-05-14 18:42:48 +0100

    add js test

---
+++ tests/js.js
@@ -0,0 +1,62 @@
+/**
+ * Comment
+ *
+ * comment comment
+ */
+
+// Comment
+
+var variable = 'string'
+var variable2 = variable
+
+let localVar = 'string'
+
+const constant = 'CONSTANT'
+
+let numOperator = 3 + 4
+
+let arr = ['one', 'two', numOperator]
+
+let obj = {
+  first: 'string',
+  second: ['one', 'two', numOperator]
+}
+
+let templateLiteral = `this is a var ${variable}`
+
+function name(param) {
+  let output = param + 'string'
+  return output
+}
+
+const funcName = param => {
+  return false
+}
+
+import React, { Component } from 'react'
+
+class ComponentName extends Component {
+  constructor() {
+    super()
+    this.state = {
+      one: two
+    }
+  }
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+const ComponentName2 = () => {
+  return (
+    <ComponentName>
+      text text text
+    </ComponentName>
+  )
+}
+
+export default ComponentName2
