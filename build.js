fd2e57cec7d65e2b1692acb7313fa95fb65e8a98
Will Stone <will@wstone.io>
2018-04-25 22:35:34 +0100

    break out colours into normal, bright, other, scopes, and ui

--- build.js
+++ build.js
@@ -1,266 +1,268 @@
 const fs = require('fs')
-var tinycolor = require('tinycolor2')
+// var tinycolor = require('tinycolor2')
 
-const baseColour = '#21252b'
-
-const colours = {
+const normal = {
+  black: '#21252b', // Shark
   blue: '#61afef', // Cornflower Blue
   cyan: '#56b6c2', // Fountain Blue
   green: '#98c379', // Olivine
   magenta: '#c678dd', // Lavender
-  orange: '#d19a66', // Whiskey
   red: '#e06c75', // Sunglo
-  yellow: '#e5c07b' // Harvest Gold
+  white: '#abb2bf', // Cadet Blue
+  yellow: '#e5c07b', // Harvest Gold
+}
+
+const bright = {
+  black: '#3e4451', // Bright Gray
+  blue: '#008cff', // Azure Radiance
+  cyan: '#00e1ff', // Cyan / Aqua
+  green: '#6aff00', // Bright Green
+  magenta: '#c300ff', // Electric Violet
+  red: '#ff0015', // Red
+  white: '#d2d6db', // Iron
+  yellow: '#ffa600', // Web Orange
+}
+
+const other = {
+  whiskey: '#d19a66',
+  shuttleGray: '#5c6370',
+  woodsmoke: '#181a1f',
+  bunker: '#080a0f',
 }
 
 const scopes = {
-  storage: colours.blue,
-  primitive: colours.cyan,
-  string: colours.green,
-  function: colours.orange,
-  tag: colours.yellow,
-  keyword: colours.red
+  storage: normal.blue,
+  primitive: normal.cyan,
+  string: normal.green,
+  function: other.whiskey,
+  tag: normal.yellow,
+  keyword: normal.red,
+  variable: bright.white,
+  text: normal.white,
+  comment: other.shuttleGray,
+  punctuation: normal.white,
 }
 
-const isDark = tinycolor(baseColour).isDark()
-
-const uiScopes = {
-  variable: isDark
-    ? tinycolor(baseColour).brighten(70).toString()
-    : tinycolor(baseColour).brighten(-70).toString(),
-  text: isDark
-    ? tinycolor(baseColour).brighten(55).toString()
-    : tinycolor(baseColour).brighten(-55).toString(),
-  comment: isDark
-    ? tinycolor(baseColour).brighten(24).toString()
-    : tinycolor(baseColour).brighten(-24).toString(),
-  uiForeground: isDark ? '#ffffff' : '#000000',
-  ui4: isDark
-    ? tinycolor(baseColour).brighten(12).toString()
-    : tinycolor(baseColour).brighten(-12).toString(),
-  ui3: baseColour,
-  ui2: isDark
-    ? tinycolor(baseColour).brighten(-5).toString()
-    : tinycolor(baseColour).brighten(5).toString(),
-  ui1: isDark
-    ? tinycolor(baseColour).brighten(-11).toString()
-    : tinycolor(baseColour).brighten(11).toString(),
-  error: tinycolor(colours.red).brighten(-10).toString(),
-  warning: tinycolor(colours.yellow).brighten(-6).toString(),
-  info: tinycolor(colours.blue).brighten(-27).toString()
+const ui = {
+  foreForeground: '#ffffff',
+  foreground: bright.black,
+  foreBackground: normal.black,
+  background: other.woodsmoke,
+  backBackground: other.bunker,
+  error: bright.red,
+  warning: bright.yellow,
+  info: bright.blue,
+  primary: normal.red,
 }
-
-console.log(tinycolor(baseColour).brighten(12).toString())
-// #40444a77
 
 const template = {
   name: 'Plastic',
   colors: {
-    'activityBar.background': uiScopes.ui3,
-    'activityBar.dropBackground': uiScopes.ui3,
-    'activityBar.foreground': uiScopes.uiForeground,
-    'activityBarBadge.background': scopes.keyword,
-    'activityBarBadge.foreground': uiScopes.uiForeground,
-    'button.background': scopes.keyword,
-    contrastBorder: uiScopes.ui1,
-    'debugToolBar.background': uiScopes.ui2,
-    'dropdown.background': uiScopes.ui2,
-    'dropdown.border': uiScopes.ui1,
-    'editor.background': uiScopes.ui3,
+    'activityBar.background': ui.foreBackground,
+    'activityBar.dropBackground': ui.background,
+    'activityBar.foreground': ui.foreForeground,
+    'activityBarBadge.background': ui.primary,
+    'activityBarBadge.foreground': ui.foreForeground,
+    'button.background': ui.primary,
+    contrastBorder: ui.backBackground,
+    'debugToolBar.background': ui.background,
+    'dropdown.background': ui.background,
+    'dropdown.border': ui.backBackground,
+    'editor.background': ui.foreBackground,
     'editor.findMatchBackground': '#42557B',
     'editor.findMatchHighlightBackground': '#314365',
-    'editor.foreground': uiScopes.text,
-    'editor.lineHighlightBackground': uiScopes.ui4 + '55',
-    'editor.selectionBackground': uiScopes.ui4,
+    'editor.foreground': normal.white,
+    'editor.lineHighlightBackground': ui.foreground + '55',
+    'editor.selectionBackground': ui.foreground,
     'editor.wordHighlightBackground': '#00000000',
     'editor.wordHighlightBorder': scopes.storage,
     'editor.wordHighlightStrongBackground': '#00000000',
-    'editor.wordHighlightStrongBorder': uiScopes.variable,
-    'editorActiveLineNumber.foreground': uiScopes.text,
+    'editor.wordHighlightStrongBorder': bright.white,
+    'editorActiveLineNumber.foreground': normal.white,
     'editorBracketMatch.border': scopes.storage,
-    'editorCursor.foreground': uiScopes.text,
-    'editorError.foreground': uiScopes.error,
-    'editorGroup.background': uiScopes.ui2,
-    'editorGroup.border': uiScopes.ui1,
-    'editorGroupHeader.tabsBackground': uiScopes.ui2,
-    'editorHoverWidget.background': uiScopes.ui2,
+    'editorCursor.foreground': normal.white,
+    'editorError.foreground': ui.error,
+    'editorGroup.background': ui.background,
+    'editorGroup.border': ui.backBackground,
+    'editorGroupHeader.tabsBackground': ui.background,
+    'editorHoverWidget.background': ui.background,
     'editorHoverWidget.border': scopes.storage,
-    'editorIndentGuide.background': uiScopes.ui4,
-    'editorInfo.foreground': uiScopes.info,
-    'editorLineNumber.foreground': uiScopes.ui4,
-    'editorOverviewRuler.border': uiScopes.ui1,
-    'editorOverviewRuler.errorForeground': uiScopes.error,
-    'editorOverviewRuler.infoForeground': uiScopes.info,
-    'editorOverviewRuler.warningForeground': uiScopes.warning,
-    'editorRuler.foreground': uiScopes.ui4,
-    'editorSuggestWidget.background': uiScopes.ui2,
+    'editorIndentGuide.background': ui.foreground,
+    'editorInfo.foreground': ui.info,
+    'editorLineNumber.foreground': ui.foreground,
+    'editorOverviewRuler.border': ui.backBackground,
+    'editorOverviewRuler.errorForeground': ui.error,
+    'editorOverviewRuler.infoForeground': ui.info,
+    'editorOverviewRuler.warningForeground': ui.warning,
+    'editorRuler.foreground': ui.foreground,
+    'editorSuggestWidget.background': ui.background,
     'editorSuggestWidget.border': scopes.storage,
-    'editorSuggestWidget.selectedBackground': uiScopes.ui3,
-    'editorWarning.foreground': uiScopes.warning,
-    'editorWhitespace.foreground': uiScopes.ui4,
-    'editorWidget.background': uiScopes.ui3,
+    'editorSuggestWidget.selectedBackground': ui.background,
+    'editorWarning.foreground': ui.warning,
+    'editorWhitespace.foreground': ui.foreground,
+    'editorWidget.background': ui.background,
     focusBorder: scopes.storage,
-    'input.background': uiScopes.ui1,
+    'input.background': ui.backBackground,
     'inputOption.activeBorder': scopes.storage,
-    'inputValidation.errorBackground': uiScopes.error,
-    'inputValidation.errorBorder': uiScopes.error,
-    'inputValidation.infoBackground': uiScopes.info,
-    'inputValidation.infoBorder': uiScopes.info,
-    'inputValidation.warningBackground': uiScopes.warning,
-    'inputValidation.warningBorder': uiScopes.warning,
-    'list.activeSelectionBackground': uiScopes.ui3,
-    'list.activeSelectionForeground': uiScopes.variable,
+    'inputValidation.errorBackground': ui.error,
+    'inputValidation.errorBorder': ui.error,
+    'inputValidation.infoBackground': ui.info,
+    'inputValidation.infoBorder': ui.info,
+    'inputValidation.warningBackground': ui.warning,
+    'inputValidation.warningBorder': ui.warning,
+    'list.activeSelectionBackground': ui.foreBackground,
+    'list.activeSelectionForeground': bright.white,
     'list.focusBackground': '#2c313a',
     'list.hoverBackground': '#292d35',
-    'list.inactiveSelectionBackground': uiScopes.ui3,
-    'notificationCenterHeader.background': uiScopes.ui2,
-    'notifications.background': uiScopes.ui2,
-    'panel.background': uiScopes.ui2,
-    'panel.border': uiScopes.ui1,
+    'list.inactiveSelectionBackground': ui.foreBackground,
+    'notificationCenterHeader.background': ui.background,
+    'notifications.background': ui.background,
+    'panel.background': ui.background,
+    'panel.border': ui.backBackground,
     'peekView.border': scopes.storage,
-    'peekViewEditor.background': uiScopes.ui2,
-    'peekViewEditor.matchHighlightBackground': uiScopes.ui4,
-    'peekViewResult.background': uiScopes.ui3,
-    'peekViewResult.matchHighlightBackground': uiScopes.ui4,
-    'peekViewResult.selectionBackground': uiScopes.ui3,
-    'peekViewResult.selectionForeground': uiScopes.text,
-    'peekViewTitle.background': uiScopes.ui3,
-    'scrollbarSlider.activeBackground': uiScopes.variable + '77',
-    'scrollbarSlider.background': uiScopes.ui4 + '99',
-    'scrollbarSlider.hoverBackground': uiScopes.ui4 + 'aa',
-    'sideBar.background': uiScopes.ui2,
-    'sideBarSectionHeader.background': uiScopes.ui3,
-    'statusBar.background': uiScopes.ui3,
-    'statusBar.debuggingBackground': uiScopes.ui3,
+    'peekViewEditor.background': ui.background,
+    'peekViewEditor.matchHighlightBackground': ui.foreground,
+    'peekViewResult.background': ui.background,
+    'peekViewResult.matchHighlightBackground': ui.foreground,
+    'peekViewResult.selectionBackground': ui.background,
+    'peekViewResult.selectionForeground': normal.white,
+    'peekViewTitle.background': ui.background,
+    'scrollbarSlider.activeBackground': bright.white + '77',
+    'scrollbarSlider.background': ui.foreground + '99',
+    'scrollbarSlider.hoverBackground': ui.foreground + 'aa',
+    'sideBar.background': ui.background,
+    'sideBarSectionHeader.background': ui.foreBackground,
+    'statusBar.background': ui.foreBackground,
+    'statusBar.debuggingBackground': ui.background,
     'statusBar.foreground': '#9da5b4',
-    'statusBar.noFolderBackground': uiScopes.ui3,
-    'statusBarItem.hoverBackground': uiScopes.ui3,
-    'tab.activeBackground': uiScopes.ui3,
-    'tab.activeForeground': uiScopes.text,
-    'tab.border': uiScopes.ui1,
-    'tab.inactiveBackground': uiScopes.ui2,
-    'tab.inactiveForeground': uiScopes.comment,
-    'terminal.ansiBlack': uiScopes.ui3,
+    'statusBar.noFolderBackground': ui.background,
+    'statusBarItem.hoverBackground': ui.background,
+    'tab.activeBackground': ui.background,
+    'tab.activeForeground': normal.white,
+    'tab.border': ui.backBackground,
+    'tab.inactiveBackground': ui.background,
+    'tab.inactiveForeground': bright.black,
+    'terminal.ansiBlack': ui.background,
     'terminal.ansiBlue': scopes.storage,
-    'terminal.ansiBrightBlack': uiScopes.comment,
+    'terminal.ansiBrightBlack': bright.black,
     'terminal.ansiBrightBlue': '#008cff',
     'terminal.ansiBrightCyan': '#00e1ff',
     'terminal.ansiBrightGreen': '#6aff00',
     'terminal.ansiBrightMagenta': '#c300ff',
     'terminal.ansiBrightRed': '#ff0015',
-    'terminal.ansiBrightWhite': uiScopes.variable,
+    'terminal.ansiBrightWhite': bright.white,
     'terminal.ansiBrightYellow': '#ffa600',
     'terminal.ansiCyan': scopes.primitive,
     'terminal.ansiGreen': scopes.string,
     'terminal.ansiMagenta': '#C678DD',
-    'terminal.ansiRed': scopes.keyword,
-    'terminal.ansiWhite': uiScopes.text,
-    'terminal.ansiYellow': scopes.tag,
-    'terminal.foreground': uiScopes.text,
-    'titleBar.activeBackground': uiScopes.ui3,
-    'titleBar.activeForeground': uiScopes.variable,
-    'titleBar.inactiveBackground': uiScopes.ui3,
-    'titleBar.inactiveForeground': uiScopes.text,
-    'widget.shadow': uiScopes.ui1
+    'terminal.ansiRed': normal.red,
+    'terminal.ansiWhite': normal.white,
+    'terminal.ansiYellow': normal.yellow,
+    'terminal.foreground': scopes.text,
+    'titleBar.activeBackground': ui.foreBackground,
+    'titleBar.activeForeground': bright.white,
+    'titleBar.inactiveBackground': ui.foreBackground,
+    'titleBar.inactiveForeground': normal.white,
+    'widget.shadow': ui.backBackground,
   },
   tokenColors: [
     {
       name: 'Punctuation',
       scope: ['meta.brace', 'punctuation'],
       settings: {
-        foreground: uiScopes.text
-      }
+        foreground: scopes.punctuation,
+      },
     },
     {
       name: 'Variable',
       scope: ['support.type', 'support.variable', 'variable'],
       settings: {
-        foreground: uiScopes.variable
-      }
+        foreground: scopes.variable,
+      },
     },
     {
-      name: 'Blue',
+      name: 'Storage',
       scope: ['storage', 'support.class'],
       settings: {
-        foreground: colours.blue
-      }
+        foreground: scopes.storage,
+      },
     },
     {
       name: 'Primitive',
       scope: ['constant', 'keyword.other'],
       settings: {
-        foreground: colours.cyan
-      }
+        foreground: scopes.primitive,
+      },
     },
     {
       name: 'Comment',
       scope: [
         'comment',
-        'punctuation.definition.comment' // start and end of comment blocks
+        'punctuation.definition.comment', // start and end of comment blocks
       ],
       settings: {
-        foreground: uiScopes.comment
-      }
+        foreground: scopes.comment,
+      },
     },
     {
-      name: 'Green',
+      name: 'String',
       scope: ['string', 'markup.inline'],
       settings: {
-        foreground: colours.green
-      }
+        foreground: scopes.string,
+      },
     },
     {
-      name: 'Orange',
+      name: 'Function',
       scope: [
         'entity',
         'support.function',
-        'punctuation.definition.entity' // css . and # for classes and IDs
+        'punctuation.definition.entity', // css . and # for classes and IDs
       ],
       settings: {
-        foreground: colours.orange
-      }
+        foreground: scopes.function,
+      },
     },
     {
-      name: 'Yellow',
+      name: 'Tag',
       scope: [
         'entity.name.tag',
-        'variable.language' // this
+        'variable.language', // this
       ],
       settings: {
-        foreground: colours.yellow
-      }
+        foreground: scopes.tag,
+      },
     },
     {
-      name: 'Red',
+      name: 'Keyword',
       scope: [
         'entity.name.section', // markdown headings' text
         'keyword',
         'punctuation.definition.heading', // markdown headings' hashes
-        'punctuation.definition.keyword' // css @ in @media queries
+        'punctuation.definition.keyword', // css @ in @media queries
       ],
       settings: {
-        foreground: colours.red
-      }
+        foreground: scopes.keyword,
+      },
     },
     {
       name: 'Bold',
       scope: [
         'markup.bold',
         'punctuation.definition.bold',
-        'entity.other.attribute-name.id' // css ID
+        'entity.other.attribute-name.id', // css ID
       ],
       settings: {
-        fontStyle: 'bold'
-      }
+        fontStyle: 'bold',
+      },
     },
     {
       name: 'Italic',
       scope: ['comment', 'markup.italic', 'punctuation.definition.italic'],
       settings: {
-        fontStyle: 'italic'
-      }
-    }
-  ]
+        fontStyle: 'italic',
+      },
+    },
+  ],
 }
 
 const json = JSON.stringify(template, null, 2)
