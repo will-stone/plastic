9fa9b787a0761e54cb6e83964550a59f7dc1c0b3
Will Stone <will@wstone.io>
2018-05-22 18:26:35 +0100

    use plastic v1 colours

--- config/colours.mjs
+++ config/colours.mjs
@@ -8,22 +8,22 @@ export default {
   AZURE_RADIANCE: '#007FFF',
   BRIGHT_GRAY: '#3C4151',
   BRIGHT_GREEN: '#66FF00',
+  BRIGHT_TURQUOISE: '#08E8DE',
   BUNKER: '#0D1117',
   CADET_BLUE: '#A9B2C3',
   CINNABAR: '#E34234',
-  CYAN_AQUA: '#00FFFF',
+  CORNFLOWER_BLUE: '#61afef', // approx.
   ELECTRIC_VIOLET: '#8B00FF',
-  FOUNTAIN_BLUE: '#56B4BE',
-  HARVEST_GOLD: '#E0B974',
-  HAVELOCK_BLUE: '#5590D9',
+  FOUNTAIN_BLUE: '#56B6C2', // approx.
+  HARVEST_GOLD: '#E5C07B', // approx.
   IRON: '#D4D7D9',
   LAVENDER: '#B57EDC',
-  OLIVINE: '#9AB973',
+  OLIVINE: '#98C379', // approx.
   SHARK: '#21252b', // approx.
   SHUTTLE_GRAY: '#5F6672',
-  SUNGLO: '#e16865',
+  SUNGLO: '#E06C75', // approx.
   TRANSPARENT: '#00000000',
   TRUE_WHITE: '#ffffffff',
-  WHISKEY: '#D59A6F',
+  WHISKEY: '#D19A66', // approx.
   WOODSMOKE: '#181a1f', // approx.
 };
