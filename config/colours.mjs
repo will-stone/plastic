57a7187706d6b46e4abb845cfc1c225842b05729
Will Stone <will@wstone.io>
2018-05-22 18:28:39 +0100

    use uppercase hex

--- config/colours.mjs
+++ config/colours.mjs
@@ -12,18 +12,18 @@ export default {
   BUNKER: '#0D1117',
   CADET_BLUE: '#A9B2C3',
   CINNABAR: '#E34234',
-  CORNFLOWER_BLUE: '#61afef', // approx.
+  CORNFLOWER_BLUE: '#61AFEF', // approx.
   ELECTRIC_VIOLET: '#8B00FF',
   FOUNTAIN_BLUE: '#56B6C2', // approx.
   HARVEST_GOLD: '#E5C07B', // approx.
   IRON: '#D4D7D9',
   LAVENDER: '#B57EDC',
   OLIVINE: '#98C379', // approx.
-  SHARK: '#21252b', // approx.
+  SHARK: '#21252B', // approx.
   SHUTTLE_GRAY: '#5F6672',
   SUNGLO: '#E06C75', // approx.
   TRANSPARENT: '#00000000',
   TRUE_WHITE: '#ffffffff',
   WHISKEY: '#D19A66', // approx.
-  WOODSMOKE: '#181a1f', // approx.
+  WOODSMOKE: '#181A1F', // approx.
 };
