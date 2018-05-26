3e9d3b2285f869d24b7db0c372403d2db96e8694
Will Stone <654103+will-stone@users.noreply.github.com>
2018-05-26 23:23:18 +0100

    yaml config files backup

--- src/build.js
+++ src/build.js
@@ -1,11 +1,8 @@
-import fs from 'fs';
-import {
-  colours as tokenColours,
-  styles as tokenStyles,
-} from '../config/syntax';
-import workbenchColours from '../config/workbench';
-import readThemeYaml from './readThemeYaml';
-import test from './test';
+// import fs from 'fs';
+// import { colours as tokenColours, styles as tokenStyles } from './syntax';
+// import workbenchColours from './workbench';
+import readThemeYaml from './utils/readThemeYaml';
+import test from './utils/test';
 
 const themeFile = 'theme.yaml';
 const theme = readThemeYaml(themeFile);
@@ -28,52 +25,58 @@ test(name.startsWith('Plastic'))
   .pass('Name begins with "Plastic".')
   .fail('Theme name must begin with "Plastic"');
 
-const template = {
-  name: 'Plastic v2 Beta',
-  colors: {
-    // Workbench colours
-    ...Object.keys(workbenchColours).reduce((acc, workbenchColour) => {
-      const scopes = workbenchColours[workbenchColour];
-      return {
-        ...acc,
-        ...scopes.reduce((acc, scope) => {
-          return {
-            ...acc,
-            [scope]: workbenchColour,
-          };
-        }, {}),
-      };
-    }, {}),
-  },
-  tokenColors: [
-    // Syntax colours
-    ...Object.keys(tokenColours).map(tokenColour => {
-      const scopes = tokenColours[tokenColour];
-      return {
-        name: tokenColour,
-        scope: scopes,
-        settings: {
-          foreground: tokenColour,
-        },
-      };
-    }, {}),
-    // Syntax styles
-    ...Object.keys(tokenStyles).map(tokenStyle => {
-      const scopes = tokenStyles[tokenStyle];
-      return {
-        name: tokenStyle,
-        scope: scopes,
-        settings: {
-          fontStyle: tokenStyle,
-        },
-      };
-    }, {}),
-  ],
-};
+const internalColours = readThemeYaml('src/colours.yaml');
+console.log({ ...internalColours, ...theme.colours });
 
-const json = JSON.stringify(template, null, 2);
+// const workbench = readThemeYaml('src/workbench.yaml');
+// console.log(workbench);
 
-fs.writeFile('out/plastic-theme-v2-beta.json', json, 'utf8', () =>
-  /* eslint-disable no-console, no-undef */
-  console.log('done')
-);
+// const template = {
+//   name: 'Plastic v2 Beta',
+//   colors: {
+//     // Workbench colours
+//     ...Object.keys(workbenchColours).reduce((acc, workbenchColour) => {
+//       const scopes = workbenchColours[workbenchColour];
+//       return {
+//         ...acc,
+//         ...scopes.reduce((acc, scope) => {
+//           return {
+//             ...acc,
+//             [scope]: workbenchColour,
+//           };
+//         }, {}),
+//       };
+//     }, {}),
+//   },
+//   tokenColors: [
+//     // Syntax colours
+//     ...Object.keys(tokenColours).map(tokenColour => {
+//       const scopes = tokenColours[tokenColour];
+//       return {
+//         name: tokenColour,
+//         scope: scopes,
+//         settings: {
+//           foreground: tokenColour,
+//         },
+//       };
+//     }, {}),
+//     // Syntax styles
+//     ...Object.keys(tokenStyles).map(tokenStyle => {
+//       const scopes = tokenStyles[tokenStyle];
+//       return {
+//         name: tokenStyle,
+//         scope: scopes,
+//         settings: {
+//           fontStyle: tokenStyle,
+//         },
+//       };
+//     }, {}),
+//   ],
+// };
+
+// const json = JSON.stringify(template, null, 2);
+
+// fs.writeFile('out/plastic-theme-v2-beta.json', json, 'utf8', () =>
+//   /* eslint-disable no-console, no-undef */
+//   console.log('done')
+// );
