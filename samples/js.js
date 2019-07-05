d724c6b0dad2993b3fd440a99a875889e502eb3c
Will Stone <654103+will-stone@users.noreply.github.com>
2019-07-05 13:46:21 +0100

    cleanup: prettier

--- samples/js.js
+++ samples/js.js
@@ -7,77 +7,77 @@
 
 // Comment
 
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-var variable = 'string';
-var variable2 = variable;
-var myRe = /d(b+)d/g;
+var variable = 'string'
+var variable2 = variable
+var myRe = /d(b+)d/g
 
-const json = JSON.stringify(variable, null, 2);
+const json = JSON.stringify(variable, null, 2)
 
-let localVar = 'string';
+let localVar = 'string'
 
-const constant = 'CONSTANT';
+const constant = 'CONSTANT'
 
-let numOperator = (3 + 4) / 2;
-const numPlus = numOperator++;
+let numOperator = (3 + 4) / 2
+const numPlus = numOperator++
 
-let arr = ['one', 'two', numOperator];
+let arr = ['one', 'two', numOperator]
 
 let obj = {
   first: 'string',
   second: ['one', 'two', numOperator],
-};
+}
 
-let templateLiteral = `this is a var ${variable}`;
+let templateLiteral = `this is a var ${variable}`
 
 if (localVar) {
-  console.log('log');
+  console.log('log')
 } else {
-  console.log('log');
-  console.log(window.location.hash);
+  console.log('log')
+  console.log(window.location.hash)
 }
 
 function name(param) {
-  let output = param + 'string';
-  return output;
+  let output = param + 'string'
+  return output
 }
 
 const funcName = param => {
-  return false;
-};
+  return false
+}
 
 class ComponentName extends Component {
   static propTypes = {
     param: PropTypes.string,
     children: PropTypes.node,
-  };
+  }
 
   constructor() {
-    super();
+    super()
     this.state = {
       one: 'two',
-    };
+    }
   }
 
   classProperty = () => {
-    const test = 'test';
-    return test;
-  };
+    const test = 'test'
+    return test
+  }
 
   handleClick() {
-    return false;
+    return false
   }
 
   render() {
-    const { one: value } = this.state;
-    const { param } = this.props;
+    const { one: value } = this.state
+    const { param } = this.props
 
     // some comments
-    const two = name(param, param, { key: value });
+    const two = name(param, param, { key: value })
 
-    console.log(two);
+    console.log(two)
 
     return (
       <div>
@@ -85,18 +85,18 @@ class ComponentName extends Component {
         <button onClick={this.handleClick}>Click Me</button>
         <ComponentName2 prop1={funcName('thing')} />
       </div>
-    );
+    )
   }
 }
 
 const ComponentName2 = () => {
-  return <ComponentName>text text text</ComponentName>;
-};
+  return <ComponentName>text text text</ComponentName>
+}
 
 ComponentName2.propTypes = {
   prop1: PropTypes.string,
-};
+}
 
-const arr = new Array();
+const arr = new Array()
 
-export default ComponentName2;
+export default ComponentName2
--- samples/tsx.tsx
+++ samples/tsx.tsx
@@ -1,59 +1,59 @@
-import * as React from 'react';
-import * as DOM from 'react-dom';
+import * as React from 'react'
+import * as DOM from 'react-dom'
 
-const root = document.getElementById('app');
+const root = document.getElementById('app')
 
 function test(hello) {
-  console.log(hello);
+  console.log(hello)
 }
 
-test();
+test()
 
-const bye = () => console.log('bye');
+const bye = () => console.log('bye')
 
 type Props = {
-  vehicleType: 'Car' | 'Truck' | 'Motorcycle';
-  color: string;
-  numWheels: 2 | 4;
-  age: number;
-};
-type State = {};
+  vehicleType: 'Car' | 'Truck' | 'Motorcycle'
+  color: string
+  numWheels: 2 | 4
+  age: number
+}
+type State = {}
 
 class Main extends React.Component<Props, State> {
   static propTypes = {
     prop1: PropTypes.number,
-  };
+  }
 
   constructor(props: Props) {
-    super(props);
+    super(props)
 
-    this.test = props.test;
+    this.test = props.test
   }
 
   classProperty = ({ hello }, bye) => {
-    return hello;
-  };
+    return hello
+  }
 
   render() {
-    <div>
+    ;<div>
       <div>VehicleType: {this.props.vehicleType}</div>
       <div>Color: {this.props.color}</div>
       <div>Wheels: {this.props.numWheels}</div>
       <div>Age: {this.props.age}</div>
-    </div>;
+    </div>
   }
 }
 
 interface WelcomeProps {
-  name?: string; // Change the required prop to an optional prop.
+  name?: string // Change the required prop to an optional prop.
 }
 
 const Welcome: React.SFC<WelcomeProps> = props => {
-  return <h1>Hello, {props.name}</h1>;
-};
+  return <h1>Hello, {props.name}</h1>
+}
 
 Welcome.defaultProps = {
   name: 'Guest User', // This value is adopted when name prop is omitted.
-};
+}
 
-DOM.render(<Main />, root);
+DOM.render(<Main />, root)
