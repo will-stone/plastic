7724f6bd6edf7740489d142bd819ecca226a9d63
Will Stone <654103+will-stone@users.noreply.github.com>
2021-04-07 19:54:39 +0100

    Palette emoji

--- website/pages/palette.tsx
+++ website/pages/palette.tsx
@@ -29,108 +29,120 @@ const UICard: FC<{ bg: string }> = ({ bg, children }) => {
 }
 
 const PalettePage: NextPage = () => (
-  <Layout className="space-y-8 sm:space-y-16">
-    <div className="space-y-4">
-      <img alt="" className="h-24 mx-auto" src="/images/logo.svg" />
-      <h1>Palette</h1>
-    </div>
-
-    <section className="space-y-4">
-      <h2>Syntax</h2>
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-16">
-        <SyntaxCard bg="bg-sunglo" heading="Keywords">
-          <code className="block text-sunglo">if</code>
-          <code className="block text-sunglo">import</code>
-          <code className="block text-sunglo">return</code>
-        </SyntaxCard>
-
-        <SyntaxCard bg="bg-fountainBlue" heading="Constants">
-          <code className="block text-fountainBlue">1234</code>
-          <code className="block text-fountainBlue">undefined</code>
-          <code className="block text-fountainBlue">true</code>
-        </SyntaxCard>
-
-        <SyntaxCard bg="bg-cornflowerBlue" heading="Storage">
-          <code className="block text-cornflowerBlue">const</code>
-          <code className="block text-cornflowerBlue">let</code>
-          <code className="block text-cornflowerBlue">class</code>
-        </SyntaxCard>
-
-        <SyntaxCard bg="bg-olivine" heading="String Literals">
-          <span className="block">
-            &quot;<code className="text-olivine">lorem</code>&quot;
-          </span>
-          <span className="block">
-            &apos;<code className="text-olivine">ipsum</code>&apos;
-          </span>
-          <span className="block">
-            &#96;<code className="text-olivine">dolor</code>&#96;
-          </span>
-        </SyntaxCard>
-
-        <SyntaxCard bg="bg-whiskey" heading="Attributes & Props">
-          <code className="block text-whiskey">href</code>
-          <code className="block text-whiskey">className</code>
-          <code className="block text-whiskey">src</code>
-        </SyntaxCard>
-
-        <SyntaxCard bg="bg-harvestGold" heading="Tags & Types">
-          <code className="block text-harvestGold">div</code>
-          <code className="block text-harvestGold">span</code>
-          <code className="block text-harvestGold">boolean</code>
-        </SyntaxCard>
-
-        <SyntaxCard bg="bg-lavender" heading="Function Names">
-          <code className="block">
-            <span className="text-cornflowerBlue">function</span>{' '}
-            <span className="text-lavender">add</span>() &#123;&#125;
-          </code>
-          <code className="block">
-            <span className="text-lavender">doThing</span>()
-          </code>
-          <code className="block">
-            console.<span className="text-lavender">log</span>()
-          </code>
-        </SyntaxCard>
-
-        <SyntaxCard bg="bg-shuttleGray" heading="Comments">
-          <code className="text-shuttleGray block">&#47;&#47; Comment</code>
-          <code className="text-shuttleGray block">
-            &#47;* TODO comment *&#47;
-          </code>
-          <code className="text-shuttleGray block">#!/bin/bash</code>
-        </SyntaxCard>
-
-        <SyntaxCard bg="bg-cadetBlue" heading="Text & Punctuation">
-          <code className="block">
-            &#123;&#125;()[]&quot;&apos;&#96;!&lt;&gt;
-          </code>
-          <code className="block">Lorem ipsum dolor sit amet.</code>
-        </SyntaxCard>
-
-        <SyntaxCard bg="bg-ghost" heading="Variables">
-          <code className="block">
-            <span className="text-cornflowerBlue">const</span>{' '}
-            <span className="text-ghost">aVariable</span>{' '}
-            <span className="text-sunglo">=</span>{' '}
-            <span className="text-fountainBlue">true</span>
-          </code>
-        </SyntaxCard>
-      </div>
+  <Layout className="space-y-8">
+    <section className="flex justify-center items-center space-x-6 sm:space-x-8">
+      <img alt="" className="h-24" src="/images/logo.svg" />
+      <span aria-label="" className="text-7xl" role="img">
+        🎨
+      </span>
     </section>
 
-    <section className="space-y-4">
-      <h2>UI</h2>
+    <h1>Palette</h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-16">
-        <UICard bg="bg-bunker">Borders</UICard>
-        <UICard bg="bg-woodsmoke">Background</UICard>
-        <UICard bg="bg-shark">Workspace</UICard>
-        <UICard bg="bg-dodgerBlue">Information</UICard>
-        <UICard bg="bg-robRoy">Warning</UICard>
-        <UICard bg="bg-valencia">Error</UICard>
-      </div>
+    {/* TODO Add a description of the "spec" */}
+    {/* <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+      faucibus lacus est. Pellentesque ac enim nisl. Proin eget neque convallis,
+      egestas risus eu, bibendum magna. Morbi ultricies, ex vitae facilisis
+      posuere, eros odio fringilla mauris, sit amet lacinia purus elit consequat
+      orci. Nunc consequat quam purus, id convallis urna condimentum at. Fusce
+      id mollis tortor. Nullam semper magna vel sapien faucibus auctor. Etiam
+      eget dui vitae metus interdum tempus ut a velit. Nulla efficitur magna a
+      metus fermentum posuere.
+    </p> */}
+
+    <h2>Syntax</h2>
+
+    <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-16">
+      <SyntaxCard bg="bg-sunglo" heading="Keywords">
+        <code className="block text-sunglo">if</code>
+        <code className="block text-sunglo">import</code>
+        <code className="block text-sunglo">return</code>
+      </SyntaxCard>
+
+      <SyntaxCard bg="bg-fountainBlue" heading="Constants">
+        <code className="block text-fountainBlue">1234</code>
+        <code className="block text-fountainBlue">undefined</code>
+        <code className="block text-fountainBlue">true</code>
+      </SyntaxCard>
+
+      <SyntaxCard bg="bg-cornflowerBlue" heading="Storage">
+        <code className="block text-cornflowerBlue">const</code>
+        <code className="block text-cornflowerBlue">let</code>
+        <code className="block text-cornflowerBlue">class</code>
+      </SyntaxCard>
+
+      <SyntaxCard bg="bg-olivine" heading="String Literals">
+        <span className="block">
+          &quot;<code className="text-olivine">lorem</code>&quot;
+        </span>
+        <span className="block">
+          &apos;<code className="text-olivine">ipsum</code>&apos;
+        </span>
+        <span className="block">
+          &#96;<code className="text-olivine">dolor</code>&#96;
+        </span>
+      </SyntaxCard>
+
+      <SyntaxCard bg="bg-whiskey" heading="Attributes & Props">
+        <code className="block text-whiskey">href</code>
+        <code className="block text-whiskey">className</code>
+        <code className="block text-whiskey">src</code>
+      </SyntaxCard>
+
+      <SyntaxCard bg="bg-harvestGold" heading="Tags & Types">
+        <code className="block text-harvestGold">div</code>
+        <code className="block text-harvestGold">span</code>
+        <code className="block text-harvestGold">boolean</code>
+      </SyntaxCard>
+
+      <SyntaxCard bg="bg-lavender" heading="Function Names">
+        <code className="block">
+          <span className="text-cornflowerBlue">function</span>{' '}
+          <span className="text-lavender">add</span>() &#123;&#125;
+        </code>
+        <code className="block">
+          <span className="text-lavender">doThing</span>()
+        </code>
+        <code className="block">
+          console.<span className="text-lavender">log</span>()
+        </code>
+      </SyntaxCard>
+
+      <SyntaxCard bg="bg-shuttleGray" heading="Comments">
+        <code className="text-shuttleGray block">&#47;&#47; Comment</code>
+        <code className="text-shuttleGray block">
+          &#47;* TODO comment *&#47;
+        </code>
+        <code className="text-shuttleGray block">#!/bin/bash</code>
+      </SyntaxCard>
+
+      <SyntaxCard bg="bg-cadetBlue" heading="Text & Punctuation">
+        <code className="block">
+          &#123;&#125;()[]&quot;&apos;&#96;!&lt;&gt;
+        </code>
+        <code className="block">Lorem ipsum dolor sit amet.</code>
+      </SyntaxCard>
+
+      <SyntaxCard bg="bg-ghost" heading="Variables">
+        <code className="block">
+          <span className="text-cornflowerBlue">const</span>{' '}
+          <span className="text-ghost">aVariable</span>{' '}
+          <span className="text-sunglo">=</span>{' '}
+          <span className="text-fountainBlue">true</span>
+        </code>
+      </SyntaxCard>
+    </section>
+
+    <h2>UI</h2>
+
+    <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-16">
+      <UICard bg="bg-bunker">Borders</UICard>
+      <UICard bg="bg-woodsmoke">Background</UICard>
+      <UICard bg="bg-shark">Workspace</UICard>
+      <UICard bg="bg-dodgerBlue">Information</UICard>
+      <UICard bg="bg-robRoy">Warning</UICard>
+      <UICard bg="bg-valencia">Error</UICard>
     </section>
   </Layout>
 )
