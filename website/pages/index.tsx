20cbbef3348d1339e3d9b6251ea79bd92e79f706
Will Stone <654103+will-stone@users.noreply.github.com>
2023-01-08 12:03:01 +0000

    docs: cache bust

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -68,7 +68,7 @@ const IndexPage: NextPage<{ apps: { slug: string; title: string }[] }> = ({
               <img
                 alt=""
                 className="mx-auto"
-                src={`https://raw.githubusercontent.com/will-stone/plastic/main/themes/${slug}/screenshot.png`}
+                src={`https://raw.githubusercontent.com/will-stone/plastic/main/themes/${slug}/screenshot.png?v=2`}
               />
             </div>
           </a>
