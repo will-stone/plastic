15c15871168386bb6363cf97830b470a37547778
Will Stone <654103+will-stone@users.noreply.github.com>
2025-06-03 20:58:30 +0100

    Upgrade Prettier

--- prettier.config.cjs
+++
@@ -1,6 +0,0 @@
-const prettierConfig = require('@will-stone/prettier-config')
-
-module.exports = {
-  ...prettierConfig,
-  plugins: [...prettierConfig.plugins, 'prettier-plugin-astro'],
-}
---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+import prettierConfig from '@will-stone/prettier-config'
+
+export default {
+  ...prettierConfig,
+  plugins: ['prettier-plugin-astro'],
+}
